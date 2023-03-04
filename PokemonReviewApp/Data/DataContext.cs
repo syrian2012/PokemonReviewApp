b9d8529f061aed5125d13b55f07d697ddfb79cab
mhdhaid <mhd4.hz@gmail.com>
2023-03-04 23:32:54 +0300

    before DB

---
+++ PokemonReviewApp/Data/DataContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new {pc.CategoryID,pc.PokemonID});
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(P => P.PokemonID);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(c => c.Category)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(c => c.CategoryID);
+
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(po => new { po.PokemonID, po.OwnerID });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(po => po.PokemonOwners)
+                .HasForeignKey(P => P.PokemonID);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(o => o.Owner)
+                .WithMany(po => po.PokemonOwners)
+                .HasForeignKey(o => o.OwnerID);
+        }
+    }
+}
