c78e23b55d7c8ecee0e8c10c141bcee109aaac0d
Mohammad Haidar <mhd4.hz@gmail.com>
2023-03-09 16:54:04 +0300

    review api finished

--- PokemonReviewApp/Controllers/ReviewController.cs
+++ PokemonReviewApp/Controllers/ReviewController.cs
@@ -11,11 +11,13 @@ namespace PokemonReviewApp.Controllers
     public class ReviewController : Controller
     {
         private readonly IReviewRepository _reviewRepository;
+        private readonly IPokemonRepository _pokemonRepository;
         private readonly IMapper _mapper;
 
-        public ReviewController(IReviewRepository reviewRepository, IMapper mapper)
+        public ReviewController(IReviewRepository reviewRepository,IPokemonRepository pokemonRepository , IMapper mapper)
         {
             _reviewRepository = reviewRepository;
+            _pokemonRepository = pokemonRepository;
             _mapper = mapper;
         }
 
@@ -35,7 +37,7 @@ namespace PokemonReviewApp.Controllers
         [ProducesResponseType(400)]
         public IActionResult GetReview(int reviewID)
         {
-            if(_reviewRepository.ReviewExists(reviewID))
+            if(!_reviewRepository.ReviewExists(reviewID))
                 return NotFound();
 
             var review = _mapper.Map<ReviewDto>(_reviewRepository.GetReview(reviewID));
@@ -44,5 +46,21 @@ namespace PokemonReviewApp.Controllers
 
             return Ok(review);
         }
+
+        [HttpGet("{pokeID}/pokemon")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewsOfAPokemon(int pokeID)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeID))
+                return NotFound();
+
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewRepository.GetReviewsOfAPokemon(pokeID));
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            return Ok(reviews);
+        }
+        
     }
 }
